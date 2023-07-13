88d1fceae2e1b2a1b621332787ed51ce7f2a1e45
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-13 12:33:23 -0300

    Estrutura visual - Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  padding: '4px 8px',
+  border: 0,
+  borderRadius: 8,
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
   )
 }
